cdbd0fec907c9d3cc21d34953e73ddb65b9836c3
Michal Janaszek <mjanasze@intercars.eu>
2018-08-18 11:02:16 +0200

    refactor: remove dead code

--- src/loader/parse-helpers.js
+++ src/loader/parse-helpers.js
@@ -8,79 +8,6 @@ function pushParts(source, parts, from, to) {
   parts.push(end);
 }
 
-function getKindFromStoryOfNode(object) {
-  if (object.arguments.length < 1) {
-    return "";
-  }
-
-  const kindArgument = object.arguments[0];
-
-  if (kindArgument.type === "Literal") {
-    return kindArgument.value;
-  }
-
-  if (kindArgument.type === "TemplateLiteral") {
-    // we can generate template, but it will not be a real value
-    // until the full template compilation. probably won't fix.
-    return "";
-  }
-
-  // other options may include some complex usages.
-  return "";
-}
-
-function findRelatedKind(object) {
-  if (!object || !object.callee) {
-    return "";
-  }
-
-  if (object.callee.name === STORIES_OF) {
-    return getKindFromStoryOfNode(object);
-  }
-
-  return findRelatedKind(object.callee.object);
-}
-
-function handleADD(node, parent, adds) {
-  if (
-    !node.property ||
-    !node.property.name ||
-    node.property.name.indexOf("add") !== 0
-  ) {
-    return;
-  }
-
-  const addArgs = parent.arguments;
-
-  if (!addArgs || addArgs.length < 2) {
-    return;
-  }
-
-  const storyName = addArgs[0];
-  const lastArg = addArgs[addArgs.length - 1];
-
-  if (storyName.type !== "Literal") {
-    // if story name is not literal, it much harder to extract it
-    return;
-  }
-
-  const kind = findRelatedKind(node.object) || "";
-  const key = `${kind}@${storyName.value}`;
-
-  // eslint-disable-next-line no-param-reassign
-  adds[key] = {
-    // Debug: code: source.slice(storyName.start, lastArg.end),
-    startLoc: {
-      col: storyName.loc.start.column,
-      line: storyName.loc.start.line
-    },
-    endLoc: {
-      col: lastArg.loc.end.column,
-      line: lastArg.loc.end.line
-    }
-  };
-}
-
 function handleSTORYOF(node, parts, source, lastIndex) {
   if (!node.callee || !node.callee.name || node.callee.name !== STORIES_OF) {
     return lastIndex;
@@ -91,4 +18,4 @@ function handleSTORYOF(node, parts, source, lastIndex) {
   return node.end;
 }
 
-export { handleSTORYOF, handleADD };
+export { handleSTORYOF };
